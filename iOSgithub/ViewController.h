eb02e453f4450d5f3f88fc0416df3b43d95da395
Naira <naira@Macintosh.local>
2015-11-25 20:26:33 +0400

    Initial Commit

---
+++ iOSgithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSgithub
+//
+//  Created by Naira on 11/25/15.
+//  Copyright (c) 2015 Naira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSgithub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSgithub
+//
+//  Created by Naira on 11/25/15.
+//  Copyright (c) 2015 Naira. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
